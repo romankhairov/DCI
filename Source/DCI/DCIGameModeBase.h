e8ee799b5d5a7a037b4cd9a12f32bffa713868a0
Roman Khairov <roman.khairov@gmail.com>
2023-11-07 18:01:55 +0100

    Initial commit

---
+++ Source/DCI/DCIGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DCIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DCI_API ADCIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
